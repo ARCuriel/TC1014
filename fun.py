0599af516211519bf21006cefca0e3754acd5f7c
ARCuriel <A01229982@itesm.mx>
2015-03-04 17:19:58 -0600

    WSQ03
    Here you can see how to make some kind of operations between two numbers

---
+++ fun.py
@@ -0,0 +1,14 @@
+# Aram Rodrigo Curiel Graxiola
+# A01229982
+# WSQ03
+
+print("***WELCOME TO YOUR FRIENDLY OPERATOR***")
+
+num1 = int(input("Give me the first number: "))
+num2 = int(input("Give me the second number: "))
+
+print ("- The sum of your numbers is:", num1 + num2)
+print ("- The difference of your numbers is:", num1 - num2)
+print ("- The product of your numbers is:", num1 * num2)
+print ("- The division of your numbers is:", num1 // num2)
+print ("- The remainder form the division of your numbers is:", num1 % num2)
