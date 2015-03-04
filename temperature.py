79754c8c3cbc239772d0a126d7ed795c6f7b3dfd
ARCuriel <A01229982@itesm.mx>
2015-03-04 16:16:26 -0600

    WSQ05
    This program shows how to utilize conditionals ("if" and "else")

---
+++ temperature.py
@@ -0,0 +1,17 @@
+#Aram Rodrigo Curiel Graxiola
+#A01229982
+#WSQ04
+
+print ("***WELCOME TO YOUR TEMPERATURE CONVERTER***")
+F = input (("Give me the temperature in Fahrenheit"))
+C = 5 * (F - 32)/9
+print ("The Conversion Result is", F, "Cº")
+
+if (C => 100)
+{
+print ("The water doesn't boil at this temperature")
+}
+else
+{
+print ("The water boils at this temperature")
+}
