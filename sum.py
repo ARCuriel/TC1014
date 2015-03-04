99147cbdf5f54d3226b1ef7eee6ac3d8114ad842
ARCuriel <A01229982@itesm.mx>
2015-03-04 16:22:43 -0600

    WSQ07
    An example for sum on a range of numbers

---
+++ sum.py
@@ -0,0 +1,11 @@
+# Aram Rodrigo Curiel Graxiola
+# A01229982
+# WSQ07
+
+print("***SUM RANGE***")
+x = int(input("Give me the lower bound: "))
+y = int(input("Give me the upper bound: "))
+
+sum = sum(range(x, y+1))
+print("The sum from", x,"to", y,"is:", sum)
+
